d01369b508d0dd1b0a503ee7fb4a7c8776d21a53
Muhammad Najah Yuga Desyalwa <najahyugad@gmail.com>
2024-06-08 16:29:09 +0700

    Fix: Fixed and update code in route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DaerahWisataController;
+use App\Http\Controllers\KategoriWisataController;
+use App\Http\Controllers\TempatWisataController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'dashboardAdmin']);
+
+Route::resource('daerahWisata', DaerahWisataController::class);
+Route::resource('/kategoriWisata', KategoriWisataController::class);
+Route::resource('/tempatWisata', TempatWisataController::class);
